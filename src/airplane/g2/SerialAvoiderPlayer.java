849b73200cf33ed1a5350b2719ddee5b2a269d2b
Tiernan Garsys <tgarsys@mac.com>
2013-11-12 14:43:48 -0500

    [TG] Fixed some minor issues with SerialAvoiderPlayer

--- src/airplane/g2/SerialAvoiderPlayer.java
+++ src/airplane/g2/SerialAvoiderPlayer.java
@@ -1,7 +1,13 @@
 package airplane.g2;
 
+import java.awt.geom.Line2D;
 import java.awt.geom.Point2D;
 import java.util.ArrayList;
+import java.util.Collections;
+import java.util.Comparator;
+import java.util.HashMap;
+import java.util.HashSet;
+
 import org.apache.log4j.Logger;
 import airplane.sim.Plane;
 
@@ -15,17 +21,67 @@ public class SerialAvoiderPlayer extends airplane.sim.Player {
 	private static final int WAITING = -1;
 	
 	// Dials and Knobs
-	private static final double PLANE_DIST_THRESHOLD = 20; // Distance before collision prevention occurs
-	private static final double WALL_AVOIDANCE_THRESHOLD = 10;
+	private static final double PLANE_DIST_THRESHOLD = 15; // Distance before collision prevention occurs
+	private static final double ALLOWED_PROXIMITY = 20;
+	private static final double WALL_AVOIDANCE_THRESHOLD = 5;
 	private static final float  WALL_AVOIDANCE_FORCE = 2;
 	private static final double TURN_RADIUS = 9.5; // Max degrees turned per timestep
-	private static final double YOLO_FACTOR = 0.5; // Scaling factor for tendency to ignore other planes near airport.
+	private static final double YOLO_FACTOR = 1.4; // Scaling factor for tendency to ignore other planes near airport.
 
 	private Logger logger = Logger.getLogger(this.getClass()); // for logging
+	private HashMap<Plane, Integer> startTimes;
 	
 	@Override
-	public void startNewGame(ArrayList<Plane> planes) { 
-		logger.info("Starting Game!");
+	public void startNewGame(ArrayList<Plane> planes) {
+		startTimes = new HashMap<Plane, Integer>();
+		HashSet<Plane> finished = new HashSet<Plane>();
+		
+		ArrayList<Plane> planesCopy = new ArrayList<Plane>();
+		planesCopy.addAll(planes);
+		
+		Collections.sort(planesCopy, new Comparator<Plane>() {
+			public int compare(Plane pa, Plane pb) {
+				Point2D.Double pal = pa.getLocation();
+				Point2D.Double pad = pa.getDestination();
+				Point2D.Double pbl = pb.getLocation();
+				Point2D.Double pbd = pb.getDestination();
+				return -(new java.lang.Double(pal.distance(pad)).compareTo(pbl.distance(pbd)));
+			}
+		});
+		
+		for (Plane p : planes) {
+			int proposedTime = p.getDepartureTime();
+			while (existsConflict(p, proposedTime, finished, startTimes)) {
+				proposedTime++;
+				logger.info("PROPOSED TIME: " + proposedTime);
+			}
+			
+			startTimes.put(p, proposedTime);
+			finished.add(p);
+		}
+		
+		logger.info("Starting new game!");
+	}
+
+	// TODO(TG): Generalize this to be a generic "Would I collide with this path" function
+	private boolean existsConflict(Plane p, int proposedTime, HashSet<Plane> finished, 
+			HashMap<Plane, Integer> startTimes) {
+		Line2D.Double pPath = new Line2D.Double(p.getLocation(), p.getDestination());
+		
+		for (Plane o : finished) {
+			// [TG]: Because planes move one unit per timestep, one can just use distances directly.
+			Line2D.Double oPath = new Line2D.Double(o.getLocation(), o.getDestination());
+			Point2D.Double intersect = new Point2D.Double(xIntersect(pPath, oPath), yIntersect(pPath, oPath)); 
+			double pDistance = p.getLocation().distance(intersect) - proposedTime;
+			double oDistance = o.getLocation().distance(intersect) - startTimes.get(o);
+			double oGoalDistance =  o.getLocation().distance(o.getDestination()) - startTimes.get(o);
+			// [TG]: Second clause here is to make sure that we don't freak out if 'o' would have reached its destination.
+			if (Math.abs(pDistance - oDistance) < ALLOWED_PROXIMITY && oDistance < oGoalDistance) {
+				return true;
+			}
+		}
+		
+		return false;
 	}
 	
 	@Override
@@ -36,7 +92,7 @@ public class SerialAvoiderPlayer extends airplane.sim.Player {
 		    Vector currVec = new Vector(bearings[i]);
 		    Vector goalVec = new Vector(calculateBearing(p.getLocation(), p.getDestination()));
 		    
-		    if (round <= p.getDepartureTime() 
+		    if (round <= startTimes.get(p)
 		            || p.getBearing() == FINISHED 
 		    		|| bearings[i] == WAITING && planeTooClose(p, planes)) { 
 		    	continue;
@@ -55,6 +111,8 @@ public class SerialAvoiderPlayer extends airplane.sim.Player {
 	// Calculate avoidance vector for plane-plane collisions
 	private Vector planeAvoidanceVector(Plane p, ArrayList<Plane> planes, double[] bearings) {
 		Vector outVec = new Vector(0, 0);
+		Vector currVec = new Vector(p.getBearing());
+		
 		Point2D.Double pl = p.getLocation();
 		
 		double avoidanceThreshold = Math.min(PLANE_DIST_THRESHOLD, pl.distance(p.getDestination()) * YOLO_FACTOR);
@@ -67,6 +125,7 @@ public class SerialAvoiderPlayer extends airplane.sim.Player {
 				outVec = subVectors(outVec, diff);
 				// [TG]: Causes planes to turn in opposite directions in head-on collision cases, such as on 'Double'
 				outVec = addVectors(outVec, new Vector(bearings[i]).rotateOpposite());
+				//outVec = addVectors(outVec, new Vector(bearings[i]).rotateToward(currVec, -90));
 			}
 		}
 
@@ -75,7 +134,7 @@ public class SerialAvoiderPlayer extends airplane.sim.Player {
 	
 	private boolean collisionPossible(Plane p, Plane o) {
 		// TODO(TG): Write this.
-		return true;
+		return o.getBearing() >= 0;
 	}
 
 	private Vector wallAvoidanceVector(Plane p, ArrayList<Plane> planes) {
@@ -98,9 +157,39 @@ public class SerialAvoiderPlayer extends airplane.sim.Player {
 		return false;
 	}
 	
+	private double xIntersect(Line2D l1, Line2D l2) {
+    	double x1 = l1.getX1();
+    	double x2 = l1.getX2();
+    	double x3 = l2.getX1();
+    	double x4 = l2.getX2();
+    	
+    	double y1 = l1.getY1();
+    	double y2 = l1.getY2();
+    	double y3 = l2.getY1();
+    	double y4 = l2.getY2();
+    	
+    	double denom = (x1-x2)*(y3-y4) - (y1-y2)*(x3-x4);
+    	return ((x3-x4)*(x1*y2-y1*x2)-(x1-x2)*(x3*y4-y3*x4))/denom;
+    }
+    
+    private double yIntersect(Line2D l1, Line2D l2) {
+    	double x1 = l1.getX1();
+    	double x2 = l1.getX2();
+    	double x3 = l2.getX1();
+    	double x4 = l2.getX2();
+    	
+    	double y1 = l1.getY1();
+    	double y2 = l1.getY2();
+    	double y3 = l2.getY1();
+    	double y4 = l2.getY2();
+    	
+    	double denom = (x1-x2)*(y3-y4) - (y1-y2)*(x3-x4);
+    	return ((y3-y4)*(x1*y2-y1*x2)-(y1-y2)*(x3*y4-y3*x4))/denom;
+    }
+	
 	@Override
 	public String getName() {
-		return "Avoider Player";
+		return "Serial Avoider Player";
 	}
 	
 }
